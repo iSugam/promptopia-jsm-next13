88373198943284455d450643336420a950babc73
unknown <sugamanandsahu01@gmail.com>
2023-10-04 18:15:23 +0530

    Creating database

---
+++ utils/database.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set("strictQuery", true);
+
+    if(isConnected) {
+        console.log("MongoDB is already connected");
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "share-prompt",
+            useUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        isConnected = true;
+
+        console.log("MongoDB is Connected");
+    } catch (error) {
+        console.log(error);
+    }
+
+}
